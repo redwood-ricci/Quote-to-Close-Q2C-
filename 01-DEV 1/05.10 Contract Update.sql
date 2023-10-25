b27f931b7e779e3f2abf8229805998e5c787b10b
Jim Ziller (Neocol) <jim.ziller@neocol.com>
2023-10-25 14:50:57 +0000

    Update 05.10 Contract Update.sql
    Set up to link to newly created Order

--- 01-DEV 1/05.10 Contract Update.sql
+++ 01-DEV 1/05.10 Contract Update.sql
@@ -17,93 +17,91 @@
 ---------------------------------------------------------------------------------
 -- Replicate Data
 ---------------------------------------------------------------------------------
-USE Insert_Database_Name_Here;
+USE SourceQA;
 
-EXEC SF_Refresh 'SANDBOX_QA', 'Account', 'yes'
-EXEC SF_Refresh 'SANDBOX_QA', 'PriceBook2', 'yes'
-EXEC SF_Refresh 'SANDBOX_QA', 'RecordType', 'yes'
-EXEC SF_Refresh 'SANDBOX_QA', 'sbqq__Quote__c', 'yes'
-EXEC SF_Refresh 'SANDBOX_QA', 'Opportunity', 'yes'
-EXEC SF_Refresh 'SANDBOX_QA', 'Contract', 'yes'
+EXEC SourceQA.dbo.SF_Replicate 'SANDBOX_QA','Account','PKCHUNK'
+EXEC SourceQA.dbo.SF_Replicate 'SANDBOX_QA','PriceBook2','PKCHUNK'
+EXEC SourceQA.dbo.SF_Replicate 'SANDBOX_QA','RecordType','PKCHUNK'
+EXEC SourceQA.dbo.SF_Replicate 'SANDBOX_QA','SBQQ__Quote__c','PKCHUNK'
+EXEC SourceQA.dbo.SF_Replicate 'SANDBOX_QA','Opportunity','PKCHUNK'
+EXEC SourceQA.dbo.SF_Replicate 'SANDBOX_QA','Contract','PKCHUNK'
 
 
 ---------------------------------------------------------------------------------
 -- Drop Staging Table
 ---------------------------------------------------------------------------------
-USE Insert_Database_Name_Here;
+USE StageQA;
 
 if exists (select * from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'Contract_Load' AND TABLE_SCHEMA = 'dbo')
-DROP TABLE [Contract_Load]
+DROP TABLE StageQA.dbo.[Contract_Load]
 
 ---------------------------------------------------------------------------------
 -- Create Staging Table
 ---------------------------------------------------------------------------------
-USE Insert_Database_Name_Here;
+USE StageQA;
 
-WITH Contract_CTE as 
-(
-	Select 
-	Row_Number() OVER (Partition by a.[Correct SF Account ID], a.[End Date] Order By a.[start date], a.[Product] Asc) as RowNum,
-	a.[Correct SF Account ID] as AccountID, a.[End Date] as EndDate,
-	a.*
-	FROM [Insert_SOURCE_Database_Name_Here].[dbo].[MasterSubscriptions] a
-),
-TERM_CTE as
-(
-	Select a.[Correct SF Account ID] as AccountID, a.[End Date] as EndDate, Round(Max(a.[sub term]), 0) as Term, min(a.[Start Date]) as StartDate,
-	SUM(ARR) as ARR
-	FROM [Insert_SOURCE_Database_Name_Here].[dbo].[MasterSubscriptions] a
-	GROUP BY a.[Correct SF Account ID], a.[End Date]
-)
+
 
 Select 
-	Cast('' as nvarchar(18)) as Id,
-	CAST('' as nvarchar(255)) as Error,
-	Acct.ID as AccountId, 
-	PB.Id as Pricebook2Id, 
-	Term.[StartDate] as StartDate, 
-	Term.Term as ContractTerm, 
-	'Draft' as [Status],
-	'Migrated PlanIt Contract' as [Description], 
-	RT.Id as RecordTypeId, 
-	Oppty.Id as SBQQ__Opportunity__c, 
-	Qte.Id as SBQQ__Quote__c, 
-	'true' as SBQQ__RenewalForecast__c, 
-	'true' as SBQQ__RenewalQuoted__c, 
-	'12' as SBQQ__RenewalTerm__c, 
-	'Contract Based' as RenewalModel__c, 
-	'Uplift' as RenewalPricingMethod__c, 
-	Term.[ARR] as ARR__c, 
-	'Latest End Date' as SBQQ__AmendmentRenewalBehavior__c, 
-	'true' as Auto_RUN__c, 
-	Concat(Acct.Id, ':', Convert(varchar(10), a.[End Date], 120) ) as Migration_id2__c,
-	'Neocol Migration: ' + Convert(varchar, getdate(), 121) as Migration_Status__c
-	INTO Contract_Load
-	FROM Contract_CTE a
-	LEFT OUTER JOIN Account Acct ON a.AccountID = Acct.ID 
-	LEFT OUTER JOIN Term_CTE Term ON a.AccountID = Term.AccountID and a.EndDate = Term.EndDate
-	LEFT OUTER JOIN Pricebook2 PB ON PB.[Name] = 'PowerDMS Price book (New)'
-	LEFT OUTER JOIN RecordType RT ON RT.[Name] = 'Standard Contract' and SobjectType = 'Contract'
-	LEFT OUTER JOIN sbqq__Quote__c Qte ON Concat(Acct.Id, ':', Convert(varchar(10), a.[End Date], 120)) = Qte.Migration_ID__c
-	LEFT OUTER JOIN Opportunity Oppty ON Concat(Acct.Id, ':', Convert(varchar(10), a.[End Date], 120)) = Oppty.Migration_ID__c
-	LEFT OUTER JOIN [Contract] Contr ON Concat(Acct.Id, ':', Convert(varchar(10), a.[End Date], 120)) = Contr.Migration_ID2__c
-	WHERE a.RowNum = 1
-	and Contr.ID IS NULL
-	AND isnull(a.[Ignore], '') <> '1'
-	AND a.AccountID <> '0012K00001hoG6XQAU'
+	Con.ID as Id,
+	CAST('' as nvarchar(2000)) as Error,
+	O.ID as [SBQQ__Order__c],
+
+
+	Acct.ID as REF_AccountId, 
+	Oppty.Id as REF_OpportunityID, 
+	Qte.Id as REF_QuoteID
+
+	--INTO StageQA.dbo.Contract_Load
+	FROM SourceQA.dbo.[Contract] Con
+	Inner join SourceQA.dbo.[Order] O
+		on Con.ID = O.[Order_Migration_id__c]
+	
+	LEFT JOIN SourceQA.dbo.Account Acct 
+		ON Con.AccountID = Acct.ID 
+	LEFT JOIN SourceQA.dbo.SBQQ__Quote__c Qte 
+		ON Con.SBQQ__Quote__c = Qte.ID
+	LEFT JOIN SourceQA.dbo.Opportunity Oppty 
+		ON Con.[SBQQ__Opportunity__c] = Oppty.ID
+	--WHERE
+	--AND a.AccountID <> 'xxxxxxxxxxxxx'
 	ORDER BY Acct.ID
 
 ---------------------------------------------------------------------------------
 -- Add Sort Column to speed Bulk Load performance if necessary
 ---------------------------------------------------------------------------------
-ALTER TABLE [Contract_Load]
-ADD [Sort] int IDENTITY (1,1)
+ALTER TABLE StageQA.dbo.[Contract_Load]
+ADD [Sort] int 
+GO
+WITH NumberedRows AS (
+  SELECT *, ROW_NUMBER() OVER (ORDER BY AccountID) AS OrderRowNumber
+  FROM StageQA.dbo.[Contract_Load]
+)
+UPDATE NumberedRows
+SET [Sort] = OrderRowNumber;
+
+
+---------------------------------------------------------------------------------
+-- Validations
+---------------------------------------------------------------------------------
+select ID, count(*) -- Change to migrated ID if added to object
+from StageQA.dbo.[Contract_Load]
+group by ID
+having count(*) > 1
+
+select *
+ from StageQA.dbo.[Contract_Load]
+
+
+---------------------------------------------------------------------------------
+-- Scrub
+---------------------------------------------------------------------------------
 
 ---------------------------------------------------------------------------------
 -- Load Data to Salesforce
 ---------------------------------------------------------------------------------
-USE Insert_Database_Name_Here;
-EXEC SF_Tableloader 'Upsert: bulkapi, batchsize(10)', 'SANDBOX_QA', 'Contract_Load', 'Migration_Id2__c'
+USE StageQA;
+EXEC SF_Tableloader 'UPDATE:bulkapi,batchsize(10)','SANDBOX_QA','Contract_Load'
 
 ---------------------------------------------------------------------------------
 -- Error Review	
