064ee3c69126150c18105da03f44d9231174fac0
redwood-ricci <113071501+redwood-ricci@users.noreply.github.com>
2023-11-29 20:59:38 -0500

    Update 04.01 Contract Update.sql
    Adding logic to erase products from related renewals, update their pricebook then check true on the contract preserve bundle structure

--- 01-DEV 1/04.01 Contract Update.sql
+++ 01-DEV 1/04.01 Contract Update.sql
@@ -35,6 +35,7 @@ EXEC SourceQA.dbo.SF_Replicate 'SANDBOX_QA','SBQQ__Quote__c','PKCHUNK'
 EXEC SourceQA.dbo.SF_Replicate 'SANDBOX_QA','Opportunity','PKCHUNK'
 EXEC SourceQA.dbo.SF_Replicate 'SANDBOX_QA','Contract','PKCHUNK'
 EXEC SourceQA.dbo.SF_Replicate 'SANDBOX_QA','OpportunityLineItem','PKCHUNK'
+EXEC SourceQA.dbo.SF_Replicate 'SANDBOX_QA','Order','PKCHUNK'
 
 ---------------------------------------------------------------------------------
 -- Drop Staging Table
@@ -47,10 +48,14 @@ DROP TABLE StageQA.dbo.[Contract_Load]
 ---------------------------------------------------------------------------------
 -- Create Staging Table
 ---------------------------------------------------------------------------------
-USE StageQA;
 
+DECLARE @RedwoodNewDeal2024 VARCHAR(100); -- Declares a string variable with a maximum length of 100 characters.
+DECLARE @RedwoodLegacyDeal VARCHAR(100);
+DECLARE @TieredPriceBook2023 VARCHAR(100);
 
-
+SET @RedwoodNewDeal2024 = '01sO90000008D4xIAE';
+SET @RedwoodLegacyDeal = '01sO90000008D4yIAE';
+SET @TieredPriceBook2023 = '01s3t000004H01QAAS';
 
 -- create a temp table of first orders to join with contracts later
 WITH order_rank AS (
@@ -65,14 +70,6 @@ WITH order_rank AS (
 first_orders as ( -- select the order rank and pull out only one row for each contract with the first order for that contract
 	select * from order_rank where rn = 1
 )
-
-DECLARE @RedwoodNewDeal2024 VARCHAR(100); -- Declares a string variable with a maximum length of 100 characters.
-DECLARE @RedwoodLegacyDeal VARCHAR(100);
-DECLARE @TieredPriceBook2023 VARCHAR(100);
-
-SET @RedwoodNewDeal2024 = '01sO90000008D4xIAE';
-SET @RedwoodLegacyDeal = '01sO90000008D4yIAE';
-SET @TieredPriceBook2023 = '01s3t000004H01QAAS';
 
 Select 
 	Con.ID as Id
@@ -92,6 +89,7 @@ Select
 	,Acct.ID as REF_AccountId
 	,Oppty.Id as REF_OpportunityID
 	,Qte.Id as REF_QuoteID
+	,Con.SBQQ__RenewalOpportunity__c as REF_SBQQ__RenewalOpportunity__c
 
 	INTO StageQA.dbo.Contract_Load
 	FROM SourceQA.dbo.[Contract] Con
@@ -150,17 +148,62 @@ EXEC StageQA.dbo.SF_Tableloader 'UPDATE:bulkapi,batchsize(10)','SANDBOX_QA','Con
 -----
 -- Remove all products from contract renewal opportunity
 -----
-select * from StageQA.dbo.OpportunityLineItem] where 
 
+if exists (select * from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'OpportunityLineItem_DELETE' AND TABLE_SCHEMA = 'dbo')
+DROP TABLE StageQA.dbo.OpportunityLineItem_DELETE;
+
+select
+ol.Id,
+CAST('' as nvarchar(255)) as Error
+into OpportunityLineItem_DELETE
+from SourceQA.dbo.OpportunityLineItem ol
+where ol.OpportunityId in ( -- all opportunity line items related to the contract renewals
+	select REF_SBQQ__RenewalOpportunity__c from Contract_Load
+	where REF_SBQQ__RenewalOpportunity__c is not null
+)
+
+ALTER TABLE StageQA.dbo.OpportunityLineItem_DELETE
+ADD [Sort] int IDENTITY (1,1)
+
+-- execute delete
+EXEC SF_TableLoader 'Delete','[SANDBOX_QA]','OpportunityLineItem_DELETE'
+
+-- scope errors
+select Error, count(*)
+--into StageQA.dbo.Order_DELETE2
+from StageQA.dbo.OpportunityLineItem_DELETE_Result
+where error not like '%Success%'
+group by error
 
 -----
 -- update opportunities with contract renwal pricebook ID
 -----
+-- this uses the renewal opportunity and pricebook from the contract load then pushes the pricebook to the renewal opp
 
+if exists (select * from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'Opportunity_Load' AND TABLE_SCHEMA = 'dbo')
+DROP TABLE StageQA.dbo.Opportunity_Load;
 
+select 
+REF_SBQQ__RenewalOpportunity__c as Id,
+CONCAT_WS('-',o.Migration_Opportunity__c,REF_SBQQ__RenewalOpportunity__c) as Migration_Opportunity__c, -- just concat the old migration ID with the new one to mark the change
+[SBQQ__RenewalPricebookId__c] as Pricebook2Id
+into Opportunity_Load
+from Contract_Load cl
+left join SourceQA.dbo.Opportunity o on cl.REF_SBQQ__RenewalOpportunity__c = o.Id
+where REF_SBQQ__RenewalOpportunity__c is not null
+
+USE StageQA;
+EXEC StageQA.dbo.SF_Tableloader 'UPDATE:bulkapi,batchsize(20)','SANDBOX_QA','Opportunity_Load'
+								
 --- check box ---
 
 ----------------
+
+select 
+Con.ID as Id
+,CAST('' as nvarchar(2000)) as Error
+,'true' [SBQQ__PreserveBundleStructureUponRenewals__c]
+
 
 ---------------------------------------------------------------------------------
 -- Error Review	
